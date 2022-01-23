0f977eb9d84009e8b4b5c13fe6701e68009754ce
Adrian Matyszczak <48859003+Adrian19850@users.noreply.github.com>
2022-01-23 11:06:35 +0100

    v2 calc and math

--- Cw5/src/main/java/CalculatorService.java
+++ Cw5/src/main/java/CalculatorService.java
@@ -1,6 +1,6 @@
 public interface CalculatorService {
     public double add(double input1, double input2);
-    public double subtract(double input1, double input2);
-    public double multiply(double input1, double input2);
-    public double divide(double input1, double input2);
+    public double subtract(double input3, double input4);
+    public double multiply(double input5, double input6);
+    public double divide(double input7, double input8);
 }
--- Cw5/src/main/java/MathApplication.java
+++ Cw5/src/main/java/MathApplication.java
@@ -9,15 +9,15 @@ public class MathApplication {
         return calcService.add(input1, input2);
     }
 
-    public double subtract(double input1, double input2){
-        return calcService.subtract(input1, input2);
+    public double subtract(double input3, double input4){
+        return calcService.subtract(input3, input4);
     }
 
-    public double multiply(double input1, double input2){
-        return calcService.multiply(input1, input2);
+    public double multiply(double input5, double input6){
+        return calcService.multiply(input5, input6);
     }
 
-    public double divide(double input1, double input2){
-        return calcService.divide(input1, input2);
+    public double divide(double input7, double input8){
+        return calcService.divide(input7, input8);
     }
 }
